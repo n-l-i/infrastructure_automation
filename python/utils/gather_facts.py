aca7b59cbf96e0fa49bfd086c54559cba1bf73f0
n-l-i <57808975+n-l-i@users.noreply.github.com>
2025-10-21 07:37:25 +0200

    Enable gather_facts for Arch and FreeBSD systems as well

--- python/utils/gather_facts.py
+++ python/utils/gather_facts.py
@@ -8,7 +8,14 @@ def gather_facts(host: Host) -> Host:
 
     result: Ssh_command_output = run_ssh_command(host, "whoami")
     host_data["username"] = result.stdout
-    result: Ssh_command_output = run_ssh_command(host, "hostname")
+    try:
+        result: Ssh_command_output = run_ssh_command(host, "hostname")
+    except Exception as e:
+        result: Ssh_command_output = run_ssh_command(host, "echo $HOSTNAME")
+        if not result.stdout:
+            raise Exception(
+                f"Failed to gather hostname for host '{host.host_name}'"
+            )
     host_data["host_name"] = result.stdout
     host_data |= _gather_os_data(host)
     host_data["package_managers"] = _gather_package_managers(host)
@@ -35,15 +42,38 @@ def _gather_os_data(host: Host) -> dict[str, str]:
         for line in result.stdout.split("\n")
         if (key := line.split("=")[0]) and (value := line.split("=")[1])
     }
+    host_data["os_id"] = os_release_data["ID"]
     if os_release_data["ID"] == "ubuntu":
-        host_data["os_id"] = "ubuntu"
         host_data["os_version"] = os_release_data["VERSION"].split(" ")[0]
     elif os_release_data["ID"] == "debian":
-        host_data["os_id"] = "debian"
         result: Ssh_command_output = run_ssh_command(
             host, "cat /etc/debian_version"
         )
-        host_data["os_version"] = result.stdout.strip()
+        host_data["os_version"] = result.stdout
+    elif os_release_data["ID"] == "freebsd":
+        host_data["os_version"] = os_release_data["VERSION"]
+    elif os_release_data["ID"] == "arch":
+        # Arch Linux does not have version numbers so using the date of
+        # the last full system upgrade as a proxy. If no upgrades have been
+        # performed, the first line of the pacman log is used instead. This
+        # should be the date of the initial installation.
+        try:
+            result: Ssh_command_output = run_ssh_command(
+                host,
+                'cat /var/log/pacman.log | grep "starting full system upgrade"',
+            )
+        except Exception as e:
+            result: Ssh_command_output = run_ssh_command(
+                host,
+                "head -n 1 /var/log/pacman.log",
+            )
+            if not result.stdout:
+                raise Exception(
+                    f"Failed to gather OS version for host '{host.host_name}'"
+                )
+        host_data["os_version"] = (
+            result.stdout.split("\n")[-1].split("[")[1].split("]")[0]
+        )
     else:
         print(json.dumps(os_release_data, indent=4))
         raise ValueError(
@@ -55,12 +85,20 @@ def _gather_os_data(host: Host) -> dict[str, str]:
 
 def _gather_package_managers(host: Host) -> list[str]:
     package_managers = []
-    for pkg_mgr in ["apt", "snap", "pacman", "apk", "dnf", "yum"]:
+    for package_manager, package_manager_test_command in {
+        "apt": "apt list --installed",
+        "snap": "snap list",
+        "pacman": "pacman -Q",
+        "apk": "apk info",
+        "dnf": "dnf list installed",
+        "yum": "yum list installed",
+        "pkg": "pkg info",
+    }.items():
         try:
             result: Ssh_command_output = run_ssh_command(
-                host, f"which {pkg_mgr}"
+                host, package_manager_test_command
             )
-            package_managers.append(pkg_mgr)
+            package_managers.append(package_manager)
         except Exception as e:
             continue
 
