995f2ced23ceaab1695ecc1663777e24f1e6a52c
Nommisötiduois <stisenochnommisen@megaduon.se>
2025-11-07 06:20:17 +0100

    Fix snap store changed status

--- python/modules/system_update/snap.py
+++ python/modules/system_update/snap.py
@@ -4,4 +4,4 @@ from utils.ssh import Ssh_command_output, run_ssh_command
 
 def ensure_snap_packages_are_up_to_date(host: Host) -> bool:
     result: Ssh_command_output = run_ssh_command(host, "sudo snap refresh")
-    return result.stdout != "All snaps up to date."
+    return result.stderr != "All snaps up to date."
