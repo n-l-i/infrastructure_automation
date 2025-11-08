ff11309cff821ccce17364d75c57f393c020cbd6
n-l-i <57808975+n-l-i@users.noreply.github.com>
2025-11-08 17:03:58 +0100

    Rewrite ping and become play

--- python/playbooks/ping_and_become.py
+++ python/playbooks/ping_and_become.py
@@ -1,13 +1,52 @@
-from modules.ping_and_become import ping_and_become
 from playbooks._playbooks import Play
+from modules._modules import Module_function_result, State
+from modules import ping as _ping
+from utils.inventory import Host
+from utils.ssh import Ssh_command_output, run_ssh_command as _run_ssh_command
 
 
 def play() -> Play:
-    steps = []
-    for _ in range(3):
-        steps.append(ping_and_become)
     return Play(
         name="ping_and_become",
-        steps=steps,
+        steps=[
+            _ping,
+            _test_non_become_command,
+            _test_become_command,
+        ],
         results={},
+    )
+
+
+def _test_non_become_command(host: Host) -> Module_function_result[None]:
+    try:
+        result: Ssh_command_output = _run_ssh_command(host, "whoami")
+    except Exception as e:
+        raise Exception(
+            f"Failed to run command on host '{host.host_name}': {e}"
+        ) from e
+    assert (
+        result.stdout.strip() == host.username
+    ), f'Expected user "{host.username}" != actual user "{result.stdout.strip()}"'
+
+    return Module_function_result(
+        state=State.UNCHANGED,
+        return_value=None,
+    )
+
+
+def _test_become_command(host: Host) -> Module_function_result[None]:
+    try:
+        result: Ssh_command_output = _run_ssh_command(host, "sudo whoami")
+    except Exception as e:
+        raise Exception(
+            f"Failed to run command on host '{host.host_name}': {e}"
+        ) from e
+    expected_root_user = "root"
+    assert (
+        result.stdout.strip() == expected_root_user
+    ), f'Expected user "{expected_root_user}" != actual user "{result.stdout.strip()}"'
+
+    return Module_function_result(
+        state=State.UNCHANGED,
+        return_value=None,
     )
